50cabc46cc5585f5650a7086a9eb996a24031c2f
Stanislav Khatko <s.a.hatko@gmail.com>
2018-04-30 21:57:07 +0200

    Added online games

--- Themes/apps_theme_1/views/frontend/content-page.blade.php
+++ Themes/apps_theme_1/views/frontend/content-page.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $item->name }}</title>
+    <title>{{ $item->titleTranslated }}</title>
     <style>
         body {
             min-height: 100vh;
@@ -21,7 +21,8 @@
     </style>
 </head>
 <body>
-<iframe src="{{ $item->download['link'] }}" frameborder="0"></iframe>
-
+<iframe src="{{ $item->download['link'] }}" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
 </body>
 </html>
