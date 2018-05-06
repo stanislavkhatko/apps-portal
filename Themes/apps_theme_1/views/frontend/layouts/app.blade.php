084190875d008eadbf8d25acf3783babaebd001e
Stanislav Khatko <s.a.hatko@gmail.com>
2018-05-06 20:51:11 +0200

    Added search

--- Themes/apps_theme_1/views/frontend/layouts/app.blade.php
+++ Themes/apps_theme_1/views/frontend/layouts/app.blade.php
@@ -177,6 +177,21 @@
             </a>
 
             <ul class="nav navbar-nav navbar-right app-header-subnav">
+
+                <!-- Search field -->
+                <li class="search_group">
+                    <form action="/search" method="POST" class="form-group">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="search" placeholder="@lang('portal.search')" required
+                                   minlength="5">
+                            <span class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                    </span>
+                        </div>
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+
                 <!-- Authentication Links -->
                 @if(! session()->has('subscription'))
                     <li>
