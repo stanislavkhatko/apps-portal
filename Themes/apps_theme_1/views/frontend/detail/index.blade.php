50e17bb7809bcb24e9ec3f055aef03cb2dcb59b9
Stanislav Khatko <s.a.hatko@gmail.com>
2018-05-09 12:57:30 +0200

    Styles

--- Themes/apps_theme_1/views/frontend/detail/index.blade.php
+++ Themes/apps_theme_1/views/frontend/detail/index.blade.php
@@ -2,28 +2,32 @@
 
 @section('content')
 
-    <div class="app-item">
 
-        <div class="app-item-header">
+    <div class="app-category-header">
 
-            @include('frontend.partials.back')
+        @include('frontend.partials.back')
 
-            <div class="app-item-header__title">
-                {{ $item->category->contenttype->label }} - {{ $item->category->label }}
-            </div>
-
-            @if (session()->has('downloaderror'))
-                <div class="app-item-header__error">
-                    {{ session('downloaderror') }}
-                </div>
-            @endif
+        <div class="app-category-header__title">
+            {{ $item->category->contenttype->label }} - {{ $item->category->label }}
         </div>
 
+    </div>
 
-        <a href="{{ route('view.contentitem', [$item]) }}" class="app-item-body">
+    <div class="app-item">
+
+        @if (session()->has('downloaderror'))
+            <div class="app-item-error">
+                {{ session('downloaderror') }}
+            </div>
+        @endif
+
+        <div class="app-item-body">
 
             <div class="app-item-body--main">
                 <img src="{{ $item->preview}}" alt="{{ $item->title }}" class="app-item-body__thumb">
+            </div>
+
+            <div class="app-item-body--details">
 
                 <div class="app-item-body__title">
                     {{ $item->title }}
@@ -32,10 +36,6 @@
                 <div class="app-item-body__description">
                     {!! $item->description !!}
                 </div>
-
-            </div>
-
-            <div class="app-item-body--details">
 
                 <div class="app-item-body__category">
                     {{ $item->category->label }}
@@ -51,55 +51,45 @@
                     @lang('portal.free')
                 </div>
 
-            </div>
-
-        </a>
-
-
-        <div class="app-item-detail">
-
-            <div class="app-item-detail__title">@lang('portal.description'):</div>
-
-
-            @if($item->type === 'upload')
-                <a href="{{ route('download.contentitem', $item) }}" class="btn btn-primary app-item-detail__download">
-                    @lang('portal.download_label') <span class="glyphicon glyphicon-download-alt"></span>
-                </a>
-            @else
-                @if(strpos($item->download['link'], 'online/') !== false)
-                    <a href="{{ route('play.contentitem', $item) }}" target="_blank"
-                       class="btn btn-primary app-item-detail__download">
-                        @lang('portal.play') <span class="glyphicon glyphicon-new-window"></span>
-                    </a>
-                @elseif(strpos($item->download['link'], 'www.') !== false || strpos($item->download['link'], 'http') !== false || strpos($item->download['link'], 'https') !== false)
-                    <a href="{{ $item->download['link'] }}" target="_blank"
-                       class="btn btn-primary app-item-detail__download">
-                        @lang('portal.play') <span class="glyphicon glyphicon-new-window"></span>
+                @if($item->type === 'upload')
+                    <a href="{{ route('download.contentitem', $item) }}" class="app-item-body__download">
+                        @lang('portal.download_label')
                     </a>
                 @else
-                    <iframe width="560" height="315"
-                            src="https://www.youtube-nocookie.com/embed/{!! $item->download['link'] !!}?rel=0&amp;showinfo=0"
-                            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    @if(strpos($item->download['link'], 'online/') !== false)
+                        <a href="{{ route('play.contentitem', $item) }}" target="_blank"
+                           class="app-item-body__download">
+                            @lang('portal.play')
+                        </a>
+                    @elseif(strpos($item->download['link'], 'www.') !== false || strpos($item->download['link'], 'http') !== false || strpos($item->download['link'], 'https') !== false)
+                        <a href="{{ $item->download['link'] }}" target="_blank"
+                           class=" app-item-body__download">
+                            @lang('portal.play')
+                        </a>
+                    @endif
                 @endif
-            @endif
 
-        </div>
-
-        <div class="app-category">
-
-            <div class="app-category-header">
-                <div class="app-category-header__title">
-                    @lang('portal.also_available'):
-                </div>
-            </div>
-
-            <div class="app-category-items">
-                @foreach($item->category->contentItems->take(5) as $item)
-                    @include('frontend.partials.item', $item)
-                @endforeach
             </div>
 
         </div>
+
+    </div>
+
+
+    <div class="app-category">
+
+        <div class="app-category-header">
+            <div class="app-category-header__title">
+                @lang('portal.also_available'):
+            </div>
+        </div>
+
+        <div class="app-category-items">
+            @foreach($item->category->contentItems->take(5) as $item)
+                @include('frontend.partials.item', $item)
+            @endforeach
+        </div>
+
     </div>
 
     @include('frontend.disclaimer')
