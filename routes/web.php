b02da39aae919bd7b58dba7d5a2b2c3959e5886b
Stanislav Khatko <s.a.hatko@gmail.com>
2018-05-29 23:30:36 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,8 @@ Route::group(['middleware' => 'portal'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::get('/cache/{template}/{filename}', 'Intervention\Image\ImageCacheController@getResponse');
+
 
 Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
     
