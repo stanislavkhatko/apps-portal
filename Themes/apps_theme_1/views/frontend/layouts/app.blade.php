924702a405eff3d929801baeb81c071b9491cc46
Stanislav Khatko <s.a.hatko@gmail.com>
2018-05-29 21:54:07 +0200

    fix

--- Themes/apps_theme_1/views/frontend/layouts/app.blade.php
+++ Themes/apps_theme_1/views/frontend/layouts/app.blade.php
@@ -82,13 +82,23 @@
 
     <!-- Facebook Pixel Code -->
     <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
             'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '{{ Config::get('currentPortal')->facebook_pixel }}');
         fbq('track', 'PageView');
@@ -100,10 +110,15 @@
 
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={{ Config::get('currentPortal')->analytic_tag }}"></script>
+    <script async
+            src="https://www.googletagmanager.com/gtag/js?id={{ Config::get('currentPortal')->analytic_tag }}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', '{{ Config::get('currentPortal')->analytic_tag }}');
@@ -130,7 +145,7 @@
                 <a class="app-header-logo__link" href="{{ url('/') }}">
 
                     @if ($navbar['content']['image'])
-                        <img src="{{ $navbar['content']['image'] }}">
+                        <img src="{{ '/cache/original' . $navbar['content']['image'] }}">
                     @endif
                     {{ $navbar['content']['title'] }}
                 </a>
@@ -239,7 +254,7 @@
                    class="app-navbar-menu-item {{ Request::segment(2) == $contentType->id ? 'active' : ''  }}">
 
                     @if ($contentType->icon)
-                        <img src="{{ $contentType->icon }}" class="app-navbar-menu-item__icon"
+                        <img src="{{ '/cache/original' . $contentType->icon }}" class="app-navbar-menu-item__icon"
                              alt="{{ $contentType->label }}">
                     @endif
 
--- config/imagecache.php
+++ config/imagecache.php
@@ -55,7 +55,8 @@ return array(
         'small' => 'Intervention\Image\Templates\Small',
         'medium' => 'Intervention\Image\Templates\Medium',
         'large' => 'Intervention\Image\Templates\Large',
-        'fit185' => 'App\Filters\Fit185'
+        'fit185' => 'App\Filters\Fit185',
+        'fit489x275' => 'App\Filters\Fit489x275'
     ),
 
     /*
